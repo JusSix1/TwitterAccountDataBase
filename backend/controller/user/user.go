9adb9e07e02334d8e98c397f6cbc9683e2d647be
JusSix1 <Patcharapolwor28112544@gmail.com>
2023-03-14 21:10:38 +0700

    controller - close #9

--- backend/controller/user/user.go
+++ backend/controller/user/user.go
@@ -9,7 +9,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-// POST /users --> ใช้อยู่
+// POST /users
 func CreateUser(c *gin.Context) {
 	var user entity.User
 	var emailCheck entity.User
@@ -66,8 +66,7 @@ func CreateUser(c *gin.Context) {
 
 }
 
-// GET /user/:email --> ใช้อยู่
-
+// GET /user/:email
 func GetUser(c *gin.Context) {
 	var user entity.User
 	email := c.Param("email")
@@ -78,4 +77,67 @@ func GetUser(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"data": user})
+}
+
+// PATCH /users
+func UpdateUser(c *gin.Context) {
+	var user entity.User
+	var gender entity.Gender
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if tx := entity.DB().Where("id = ?", user.Gender_ID).First(&gender); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "gender not found"})
+		return
+	}
+
+	updateUser := entity.User{
+		FirstName:       user.FirstName,
+		LastName:        user.LastName,
+		Password:        user.Password,
+		Profile_Name:    user.Profile_Name,
+		Profile_Picture: user.Profile_Picture,
+		Gender:          gender,
+		Birthday:        user.Birthday,
+	}
+
+	// validate user
+	if _, err := govalidator.ValidateStruct(updateUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if !(user.Password[0:7] == "$2a$12$") { // เช็คว่ารหัสที่ผ่านเข้ามามีการ encrypt แล้วหรือยัง หากมีการ encrypt แล้วจะไม่ทำการ encrypt ซ้ำ
+		hashPassword, err := bcrypt.GenerateFromPassword([]byte(updateUser.Password), 12)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "error hashing password"})
+			return
+		}
+		print("HASH!!!!")
+		updateUser.Password = string(hashPassword)
+	} else {
+		print("NOT HASH!!!")
+	}
+
+	if err := entity.DB().Where("email = ?", user.Email).Updates(&updateUser).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": user})
+
+}
+
+// DELETE /users/:email
+func DeleteUser(c *gin.Context) {
+	email := c.Param("email")
+	if tx := entity.DB().Exec("DELETE FROM users WHERE email = ?", email); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": email})
 }
